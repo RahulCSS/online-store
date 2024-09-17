a2e6674b101830e8cc9fd847c98be39f9ad89a1d
Rahulcss <Rahulcs1618@gmail.com>
2024-09-17 16:58:42 +0530

    Login & Signup

---
+++ client/src/apicalls/users.js
@@ -0,0 +1,36 @@
+import {axiosInstance} from ".";
+
+//*Register
+export const RegisterUser = async (payload) => {
+    try{
+        const response = await axiosInstance.post("/api/user/register",payload);
+        return response.data;
+    }catch(err){
+        console.log(err);
+        return err;
+    }
+};
+
+//*Login
+export const LoginUser = async (payload) => {
+    try{
+        const response = await axiosInstance.post("/api/user/login",payload);
+        return response.data;
+    }catch(err){
+        return err;
+    }
+};
+
+//*Get Curretn User
+export const GetCurrentUser = async () => {
+    try{
+        const response = await axiosInstance.get("/api/user/getcurrentuser",{
+            headers:{
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        return response.data;
+    }catch(err){
+        return err;
+    }
+};
